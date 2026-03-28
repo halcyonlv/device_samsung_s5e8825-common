d514cc3e2012a94782d9d2427d55a4d2e95afc80
Bahar KURT <kurtbahartr@users.noreply.github.com>
2026-03-28 21:33:46 +0300

    Revert "s5e8825-common: common: Migrate more Soong variables to bool"
    This reverts commit 0c9ac849075c5bab469d0866472de096c80576f6.

--- common.mk
+++ common.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
     codec2.vendor.base.policy \
     codec2.vendor.ext.policy
 
-$(call soong_config_set_bool,openmax,UNSUPPORT_10BIT,true)
+$(call soong_config_set,openmax,UNSUPPORT_10BIT,true)
 
 # ConfigStore
 PRODUCT_PACKAGES += disable_configstore
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += init.s5e8825.gps.rc
 
 # Graphics
-$(call soong_config_set_bool,exynos_hwc,force_client_video,true)
+$(call soong_config_set,exynos_hwc,force_client_video,true)
 
 # Health
 PRODUCT_PACKAGES += \
